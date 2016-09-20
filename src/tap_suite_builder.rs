720357cdafc0cf23d676132619d253459d6ea277
Jonathan E. Magen <jonathan.magen@cigna.com>
2016-09-20 10:20:11 -0400

    docs
    Signed-off-by: Jonathan E. Magen <jonathan.magen@cigna.com>

--- src/tap_suite_builder.rs
+++ src/tap_suite_builder.rs
@@ -1,27 +1,49 @@
 use tap_suite::TapSuite;
 use tap_test::TapTest;
 
+/// Coordinator for constructing `TapSuite` objects using the builder pattern.
+///
+/// # Examples
+///
+/// ```
+/// use tap::tap_suite_builder::TapSuiteBuilder;
+/// use tap::tap_test_builder::TapTestBuilder;
+///
+/// let tests = vec![TapTestBuilder::new()
+///                  .name("Example TAP test")
+///                  .passed(true)
+///                  .finalize()];
+///
+/// let tap_suite_from_builder = TapSuiteBuilder::new()
+///     .name("Example TAP test suite")
+///     .tests(tests)
+///     .finalize();
+///
+/// ```
 pub struct TapSuiteBuilder {
+    /// Name of test suite
     pub name: String,
+    /// Vector of type `Vec<TapTest>` which holds the actual tests
     pub tests: Vec<TapTest>,
 }
 
 impl TapSuiteBuilder {
-    fn new() -> TapSuiteBuilder {
+    /// Produce a new builder object
+    pub fn new() -> TapSuiteBuilder {
         TapSuiteBuilder { name: "".to_string(), tests: vec![] }
     }
-
-    fn name(&mut self, s: &str) -> &mut TapSuiteBuilder {
+    /// Set the name
+    pub fn name(&mut self, s: &str) -> &mut TapSuiteBuilder {
         self.name = s.to_string();
         self
     }
-
-    fn tests(&mut self, test_vec: Vec<TapTest>) -> &mut TapSuiteBuilder {
+    /// Set the tests
+    pub fn tests(&mut self, test_vec: Vec<TapTest>) -> &mut TapSuiteBuilder {
         self.tests = test_vec;
         self
     }
-
-    fn finalize(&mut self) -> TapSuite {
+    /// Produce the configured `TapSuite` object
+    pub fn finalize(&mut self) -> TapSuite {
         TapSuite {
             name: self.name.to_string(),
             tests: self.tests.iter().map(|test| {
